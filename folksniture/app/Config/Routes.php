acbe0f70448b82895929305e112a82aba6dc4a00
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2022-04-05 17:34:19 +0700

    Berhasil login

--- folksniture/app/Config/Routes.php
+++ folksniture/app/Config/Routes.php
@@ -36,8 +36,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/home', 'Home::index');
-$routes->get('/', 'Admin::index');
+// $routes->get('/home', 'Home::index');
+$routes->get('/', 'Home::index');
 $routes->get('/admin/edit/(:segment)', 'Admin::edit/$1');
 
 /*
